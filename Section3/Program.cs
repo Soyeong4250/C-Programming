fe9e8ba184aa2b38a782fd2cc33210cdc5100060
sylee <th42500@naver.com>
2026-02-10 16:51:57 +0900

    feat: section3 학습 내용 추가

---
+++ Section3/Program.cs
@@ -0,0 +1,23 @@
+﻿Days days = Days.Sunday;
+
+if (days == Days.Sunday)
+{
+    Console.WriteLine("It's Sunday");
+}
+if (days == Days.Monday)
+{
+    Console.WriteLine("It's Monday");
+}
+Console.WriteLine(days);
+Console.ReadKey();
+
+enum Days
+{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+};
